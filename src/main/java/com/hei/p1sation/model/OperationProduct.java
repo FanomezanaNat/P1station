c5da33a675473e14ad4e30c1f194f1f471e0f070
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-05-10 08:18:09 +0300

    fix: rename to OperationProduct

---
+++ src/main/java/com/hei/p1sation/model/OperationProduct.java
@@ -0,0 +1,31 @@
+package com.hei.p1sation.model;
+
+import com.hei.p1sation.model.enums.TransactionType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OperationProduct {
+    private String id;
+    private Instant date;
+    private TransactionType type;
+    private Product product;
+    private float quantity;
+
+    public OperationProduct(String id) {
+        this.id = id;
+    }
+
+    public OperationProduct(TransactionType type, Product product, float quantity) {
+
+        this.type = type;
+        this.product = product;
+        this.quantity = quantity;
+    }
+}
