6ae9c3828d0d123c4d1abfa01c63697eaf13930a
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-05-13 01:33:14 +0300

    feat: add evaporationRate  as attributes in table

--- src/main/resources/db.migration/V0_3__Create_product_table.sql
+++ src/main/resources/db.migration/V0_3__Create_product_table.sql
@@ -1,5 +1,6 @@
 CREATE TABLE "product"(
     id VARCHAR PRIMARY KEY DEFAULT uuid_generate_v4(),
+    evaporation_rate float ,
     fuel_station_id VARCHAR REFERENCES "fuel_station"(id),
     product_template_id VARCHAR REFERENCES "product_template"(id)
 );
